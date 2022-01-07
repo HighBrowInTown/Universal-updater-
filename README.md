b428a59afc051745652fae46dadddd00ea7994b9
root <root@localhost.localdomain>
2022-01-07 06:15:28 -0500

    Mark1 Updater

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Universal-updater-
+You can use the give script to update your linux system despite your distro. 
+Too much distro to choose, however you can use this script to update your distro just by selecting the appropriate option.
+
---
+++ universal-updater.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo For a fresh install lets begin with an system update and if you wish to continue please select the appropriate option in next line.  
+echo Do you wish to upgrade - 'Yes' to continue and 'No' to exit the script. 
+read decesion
+if [ $decesion = "Yes" ]
+     then
+       echo please choose  your type of operating system from the given below options
+     else     
+       echo Thank you
+fi
+echo 'a = debian based linux'
+echo 'b = redhat based linux'
+echo 'c = arch linux'
+echo 'd = opensuse'
+echo 'e = gentoo'
+echo
+read option
+	case $option in
+a) apt update && apt upgrade -y;; 
+b) yum update && yum upgrade -y;;
+c) pacman -Syy;;
+d) emaint --auto sync && emerge --ask --verbose --update --deep --newuse @world ;;
+	esac
+echo 
+echo We have just update your application.
